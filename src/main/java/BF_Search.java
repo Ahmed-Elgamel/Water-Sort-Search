be76ad5e8c58a2a0654cb6c98847fd27708dfe9a
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-11 17:54:04 +0300

    Add functionality to the BF_Search search strategy

--- src/main/java/BF_Search.java
+++ src/main/java/BF_Search.java
@@ -5,12 +5,14 @@ public class BF_Search implements SearchStrategy {
     @Override
     public Deque<SearchTreeNode> addElements(Deque<SearchTreeNode> elements1, Deque<SearchTreeNode> elements2) {
        for (SearchTreeNode element: elements2){
-
+           elements1.addLast(element);
        }
+
+       return elements1;
     }
 
     @Override
     public SearchTreeNode getElement(Deque<SearchTreeNode> elements) {
-        return null;
+        return elements.peekFirst();
     }
 }
