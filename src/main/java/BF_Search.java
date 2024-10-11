31ef46c26b07466b6233454242e2acb9e60163f4
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-11 17:45:09 +0300

    Remove uneccessary method

--- src/main/java/BF_Search.java
+++ src/main/java/BF_Search.java
@@ -1,12 +1,6 @@
 import java.util.Queue;
 
 public class BF_Search implements SearchStrategy {
-
-    @Override
-    public void addElement(Object element) {
-
-    }
-
     @Override
     public Queue<SearchTreeNode> addElements(Queue<SearchTreeNode> elements1, Queue<SearchTreeNode> elements2) {
         return null;
