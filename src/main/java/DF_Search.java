0dd21c10702952811af1782895abcdd6bf7dd0b3
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-12 15:47:58 +0300

    DFS class setup

---
+++ src/main/java/DF_Search.java
@@ -0,0 +1,13 @@
+import java.util.Deque;
+
+public class DF_Search implements SearchStrategy{
+    @Override
+    public Deque<SearchTreeNode> addElements(Deque<SearchTreeNode> elements1, Deque<SearchTreeNode> elements2) {
+        return null;
+    }
+
+    @Override
+    public SearchTreeNode getElement(Deque<SearchTreeNode> elements) {
+        return null;
+    }
+}
