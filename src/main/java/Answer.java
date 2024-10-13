7b0c2b65ff4af19be6c8384d859dbd5c19403341
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-13 22:05:08 +0300

    Modified answer string

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -15,29 +15,25 @@ public class Answer {
     }
 
     public String toString(){
-       SearchTreeNode current = node;
+        SearchTreeNode current = node;
         Deque<String> answer =new LinkedList<>();
-        System.out.println("depth "+current.depth);
-
        while (current.parent!=null)
        {
-           String s = "pour_" + current.operator.bottle2Idx+"_"+current.operator.bottle1Idx+", "+ current.operator.bottle2+" "+current.operator.bottle1+"" +
-                   "\n" +" ------"+current+" \n";
+           String s = "pour_" + current.operator.bottle2Idx+"_"+current.operator.bottle1Idx;
            answer.add(s);
            current = current.parent;
+           if(current.parent != null)
+               answer.add(",");
 
        }
-       answer.add("Initial State: "+current+" \n");
 
-       Deque<String> answerFlipped =new LinkedList<>();
+
+       StringBuilder sb =new StringBuilder();
        while(!answer.isEmpty())
-           answerFlipped.add(answer.removeLast());
+           sb.append(answer.removeLast());
 
+       sb.append(";" + String.valueOf(node.cost));
 
-
-
-
-
-       return answerFlipped.toString();
+       return sb.toString();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,73 +8,57 @@ public class Main {
     public static void runMyTestCases() {
         // Test Case 1: Simple Separation
         String testCase1Input = "3;3;r,g,b;e,e,e;e,e,e";
-        String testCase1Output = "r,e,e;e,g,e;e,b,e";
         System.out.println("Test Case 1:");
         System.out.println("Input: " + testCase1Input);
-        System.out.println("Expected Output: " + testCase1Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase1Input, "ID", false));
         System.out.println();
 
         // Test Case 2: Already Separated
         String testCase2Input = "3;3;r,e,e;g,e,e;b,e,e";
-        String testCase2Output = "r,e,e;g,e,e;b,e,e";
         System.out.println("Test Case 2:");
         System.out.println("Input: " + testCase2Input);
-        System.out.println("Expected Output: " + testCase2Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase2Input, "BF", false));
         System.out.println();
 
         // Test Case 3: Impossible Separation
         String testCase3Input = "2;2;r,b;b,r";
-        String testCase3Output = "None";  // No valid solution exists
         System.out.println("Test Case 3:");
         System.out.println("Input: " + testCase3Input);
-        System.out.println("Expected Output: " + testCase3Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase3Input, "BF", false));
         System.out.println();
 
         // Test Case 4: Multiple Empty Bottles
         String testCase4Input = "4;3;r,g,b;r,g,e;b,e,e;e,e,e";
-        String testCase4Output = "r,e,e;g,e,e;b,e,e;e,e,e";
         System.out.println("Test Case 4:");
         System.out.println("Input: " + testCase4Input);
-        System.out.println("Expected Output: " + testCase4Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase4Input, "BF", false));
         System.out.println();
 
         // Test Case 5: Larger Capacity
         String testCase5Input = "4;4;r,r,b,y;r,y,e,e;b,y,e,e;e,e,e,e";
-        String testCase5Output = "r,e,e,e;g,e,e,e;b,e,e,e;y,e,e,e";
         System.out.println("Test Case 5:");
         System.out.println("Input: " + testCase5Input);
-        System.out.println("Expected Output: " + testCase5Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase5Input, "BF", false));
         System.out.println();
 
         // Test Case 6: Complex Separation unsolvalbel i think
         String testCase6Input = "4;4;r,b,g,y;b,r,g,e;y,b,e,e;e,e,e,e";
-        String testCase6Output = "r,e,e,e;b,e,e,e;y,e,e,e;g,e,e,e";
         System.out.println("Test Case 6:");
         System.out.println("Input: " + testCase6Input);
-        System.out.println("Expected Output: " + testCase6Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase6Input, "BF", false));
         System.out.println();
 
         // Test Case 7: Complex Separation
         String testCase7Input = "4;4;r,b,g,y;e,e,e,e;e,e,e,e;e,e,e,e";
-        String testCase7Output = "r,e,e,e;b,e,e,e;y,e,e,e;g,e,e,e";
         System.out.println("Test Case 7:");
         System.out.println("Input: " + testCase7Input);
-        System.out.println("Expected Output: " + testCase7Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase7Input, "BF", false));
         System.out.println();
 
         // Test Case 8: Complex Separation
         String testCase8Input = "4;4;y,r,e,e;y,r,e,e;o,r,e,e;g,r,e,e";
-        String testCase8Output = "r,r,e;y,y,e;e,e,e;";
         System.out.println("Test Case 8:");
         System.out.println("Input: " + testCase8Input);
-        System.out.println("Expected Output: " + testCase8Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase8Input, "ID", false));
         System.out.println();
 
@@ -83,7 +67,6 @@ public class Main {
         String testCase9Input = "5;4;" +"b,y,r,b;" + "b,y,r,r;" +"y,r,b,y;" + "e,e,e,e;" + "e,e,e,e";
         System.out.println("Test Case 9:");
         System.out.println("Input: " + testCase8Input);
-        System.out.println("Expected Output: " + testCase8Output);
         System.out.println("Our algorithm answer: " + WaterSortSearch.solve(testCase9Input, "AS2", false));
         System.out.println();
 
