33108f693c76e8f98160820f385aafbb1b52adac
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-13 22:50:14 +0300

    Remove comments

--- src/main/java/WaterSortProblem.java
+++ src/main/java/WaterSortProblem.java
@@ -1,10 +1,3 @@
 public interface WaterSortProblem extends Problem{
 
-//    public  Answer[] DF_search();
-//    public  Answer[] BF_search();
-//    public  Answer[] ID_search();
-//    public  Answer[] UC_search();
-//    public  Answer[] Greedy_search();
-//    public  Answer[] ASTAR_search();
-
 }
