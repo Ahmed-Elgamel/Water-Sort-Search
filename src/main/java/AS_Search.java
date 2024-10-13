6d5635803cb4897bb116afae622126293a52a19d
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-13 21:24:01 +0300

    Finish ASTAR search

---
+++ src/main/java/AS_Search.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class AS_Search extends HeuristicSearchStrategy{
+
+    public AS_Search(int selectedHeuristicIndex){
+        /**
+         *  Note: not the best code for initializing heuristics (could make this arraylist static for example
+         *  but will avoid becuase maybe it will cause erros in test cases)
+         */
+        heuristics = new ArrayList<>();
+        heuristics.add(new BadBottlesHeuristic());
+        heuristics.add(new BadColorsHeuristic());
+        // input is one indexed
+        this.selectedHeuristic = heuristics.get(selectedHeuristicIndex-1);
+    }
+    @Override
+    public Queue<SearchTreeNode> addElements(Queue<SearchTreeNode> oldNodes, Queue<SearchTreeNode> newNodes) {
+        PriorityQueue<SearchTreeNode> priorityQueue1 = (PriorityQueue<SearchTreeNode>) oldNodes;
+
+
+        for(SearchTreeNode newNode: newNodes){
+            priorityQueue1.add(newNode);
+        }
+        return priorityQueue1;
+    }
+
+    @Override
+    public SearchTreeNode getElement(Queue<SearchTreeNode> elements) {
+        PriorityQueue<SearchTreeNode> priorityQueue = (PriorityQueue<SearchTreeNode>) elements;
+        return priorityQueue.poll();
+    }
+
+    @Override
+    public SearchTreeNode runSearchAlgorithm(Problem problem) {
+        return GenericSearch.General_Search(problem, this);
+    }
+
+    @Override
+    public Queue<SearchTreeNode> generateDataStructure() {
+        PriorityQueue<SearchTreeNode> priorityQueue = new PriorityQueue<>(new Comparator<SearchTreeNode>() {
+            @Override
+            public int compare(SearchTreeNode o1, SearchTreeNode o2) {
+                return (o1.cost + o1.heuristic) - (o2.cost + o2.heuristic);
+            }
+        });
+        return priorityQueue;
+    }
+}
