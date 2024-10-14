b151d6bd184ec65c6165e66800418c3cea131203
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2024-10-14 18:45:35 +0300

    useless stuff

--- src/main/java/code/SearchStrategy.java
+++ src/main/java/code/SearchStrategy.java
@@ -3,10 +3,11 @@ package code;
 import java.util.Queue;
 
 public interface SearchStrategy {
+    Queue<SearchTreeNode> generateDataStructure();
     Queue<SearchTreeNode> addElements(Queue<SearchTreeNode> oldNodes, Queue<SearchTreeNode> newNodes);
     SearchTreeNode getElement(Queue<SearchTreeNode> elements);
     SearchTreeNode runSearchAlgorithm(Problem problem);
 
-    Queue<SearchTreeNode> generateDataStructure();
+
 
     }
